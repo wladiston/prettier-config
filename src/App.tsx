5f514f21d8e3e899163ace1b0fcf61364d077bb1
Wlad Paiva <desenvolvedormg@gmail.com>
2022-10-17 12:56:50 -0300

    Squashed commit of the following:
    commit 7a2151385f5ed78f5a7e615e07a597962fc19d16
    Author: Wlad Paiva <desenvolvedormg@gmail.com>
    Date:   Mon Oct 17 11:31:30 2022 -0300
    
        change stufff #4

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,25 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="This is a new Vite logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count up now is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    return (
+        <div className="App">
+            <div>
+                <a href="https://vitejs.dev" target="_blank">
+                    <img src="/vite.svg" className="logo" alt="This is a new Vite logo" />
+                </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count up now is {count}
+                </button>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p>Awesome!</p>
+        </div>
+    );
 }
 
 export default App
