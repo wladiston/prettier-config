41ecff5332b01b0d5373c021a5b9e4753a9aee78
Wlad Paiva <desenvolvedormg@gmail.com>
2022-10-17 16:15:24 -0300

    non conflict function

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+function Blahablah({ children }: { children: React.ReactNode }) {
+    return <div>{children}</div>;
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <App />
