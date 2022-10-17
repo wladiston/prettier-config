f043f0fceadd702105a2362eef4e2b94a1a7ade9
Wlad Paiva <desenvolvedormg@gmail.com>
2022-10-17 13:20:03 -0300

    adde no conflicting stuff

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+// added a non conflicting comment
+function someStuff({ param, otherParam }: { param: string; otherParam: string }) {
+    return param + otherParam;
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <App />
